da453bead1690c915fd77cd416f5346a715f11b8
cvargas <cristhian.avc21@gmail.com>
2021-03-10 05:03:09 -0500

    nuestras primeras clases

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Doctor myDoctor = new Doctor();
+        myDoctor.name = "cvargas";
+        myDoctor.showName();
+    }
+}
+
